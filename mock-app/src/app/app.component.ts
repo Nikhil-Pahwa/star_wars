705f822c7783d4d5d6f7123e61cbf0bba0bdca6a
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2017-07-31 20:27:12 +0530

    [IMPR] Minor fixes

--- mock-app/src/app/app.component.ts
+++ mock-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/forkJoin';
 
 import { StarWarsService } from './app.service';
@@ -13,13 +14,16 @@ import { Character, People, Film, Actor, EmptyActor } from '../resources';
   providers: [StarWarsService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   private characters: Character[];
   private actor: Actor = EmptyActor;
   private selectedItem: Character = null;
   private isLoaded: boolean = true;
+  private errorMsg: string = '';
 
-  constructor(private starWarService: StarWarsService) {
+  constructor(private starWarService: StarWarsService) { }
+
+  ngOnInit() {
     this.starWarService.getCharacters()
       .map(character => character.characters)
       .subscribe((data: Character[]) => this.characters = data);
@@ -29,15 +33,23 @@ export class AppComponent {
     this.selectedItem = character;
     this.isLoaded = false;
     this.actor.films = [];
+    this.errorMsg = '';
     this.starWarService.getPeople(character.url)
-      .mergeMap((people: People) => {
+      .switchMap((people: People) => {
         this.actor.name = people.name;
         return Observable.forkJoin(this.fetchFilms(people))
       })
-      .subscribe((films: Film[]) => {
+      .subscribe(
+      (films: Film[]) => {
         this.actor.films = films;
         this.isLoaded = true;
-      });
+      },
+      err => {
+        console.log('error occur while fetching data');
+        this.isLoaded = true;
+        this.errorMsg = 'No record found';
+      }
+      );
   }
 
   private fetchFilms(people: People) {
